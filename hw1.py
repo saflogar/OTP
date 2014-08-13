b2322af6238383078001e6983bbe51a7a6bfae08
Sergio Flores <safgflores@hotmail.com>
2014-08-13 18:50:54 -0500

    added terminal commands

--- hw1.py
+++ hw1.py
@@ -7,45 +7,45 @@ import getopt
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 ##print 'Argument List:', str(sys.argv)
 
-
 def genRandom (padLength,numKey):
-    file = open('keys','wb')
+    keyfile = open('keys','wb')
     i = 0
     print numKey
     while i < int(numKey):
         key=''
         for j in range(0,int(padLength)):
             key = key + random.choice(string.ascii_letters)
-        file.write(key+'\n')
+        keyfile.write(key+'\n')
         print "ord"+ key
         i+=1
         print 'i:',i
-    file.close()
-    
+    keyfile.close()
 
+def xorC (m,k):
+    #we use the same method for encription and decription of the message
+    return "".join(chr(ord(a)^ord(b)) for a,b in zip (k,m))
+    
 def cipher(message):
-    file = open ('keys','r')
-    key=file.readline()
-    #zip(key,message)
-    #print 'message='+message
-    #print 'key='+key
-    #cipherText = ''
-    #for a,b in zip(key,message):
-    #   print 'a='+a+'b='+b
-    #  v=(ord(a) ^ ord(b))%27
-    # cipherText = cipherText + chr(v)
-    #print 'cipher='+ cipherText
-    return "".join(chr(ord(a)^ord(b)) for a,b in zip (key,message))
-       # print v
-       # print 'des message:'+chr((v ^ ord(b)))
+    keyfile = open ('keys','r')
+    key = keyfile.readline()
+    messageFile = open('messages','a')
+    messageFile.write(xorC(key,message)+'\n')
+    messageFile.close()   
     
+def decipher():
+    keyfile = open('keys','r')
+    key = keyfile.readline()
+    messageFile = open('messages','r')
+    cMessage = messageFile.readline()
+    message = xorC(key,cMessage)
     
+    print message
 
 def main(argv):
     print 'Number of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
     try:
-        opts, args = getopt.getopt(argv,"he:k:",["encrypt=","genKey="])
+        opts, args = getopt.getopt(argv,"he:k:d",["encrypt=","genKey="])
     except getopt.GetoptError:
         print 'otp -e <encrypt> -k <genKey>'
         sys.exit(2)
@@ -53,16 +53,17 @@ def main(argv):
         if opt == '-h':
             print 'otp -e <encrypt> -k <generatekey>'
             sys.exit()
+        elif opt == '-d':
+            decipher()
         elif opt in ("-e", "--encrypt"):
             print 'encrypt'
-            #           inputfile = arg
+            message = arg 
+            cipher(message)
+    #       inputfile = arg
         elif opt in ("-k", "--key"):
+            padLength = arg
+            numKeys = argv[2]
             print 'generate key'
-      #          outputfile = arg
-   #print 'Input file is "', inputfile
-   #print 'Output file is "', outputfile
-    #padLength= argv[1]
-    #numKey = argv[2]
-#    genRandom(padLength,numKey)
-   # print cipher(cipher('hola'))
+            print 'padlength='+padLength+'numkeys='+numKeys
+            genRandom(padLength,numKeys)
 if __name__ == "__main__":main(sys.argv[1:])# with if
