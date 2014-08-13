12e1b468a30d84c5acc77a278c3bee03b3d603d3
saflores <saflores@idt-professionals.com>
2014-08-13 14:27:30 -0700

    Added command line arguments

--- hw1.py
+++ hw1.py
@@ -3,6 +3,7 @@ import sys
 import os
 import random
 import string
+import getopt
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 ##print 'Argument List:', str(sys.argv)
 
@@ -20,20 +21,21 @@ def genRandom (padLength,numKey):
         i+=1
         print 'i:',i
     file.close()
-        
+    
 
 def cipher(message):
     file = open ('keys','r')
     key=file.readline()
     #zip(key,message)
-    print 'message='+message
-    print 'key='+key
-    cipherText = ''
-    for a,b in zip(key,message):
-        print 'a='+a+'b='+b
-        v=(ord(a) ^ ord(b))%27
-        cipherText = cipherText + chr(v)
-    print 'cipher='+ cipherText
+    #print 'message='+message
+    #print 'key='+key
+    #cipherText = ''
+    #for a,b in zip(key,message):
+    #   print 'a='+a+'b='+b
+    #  v=(ord(a) ^ ord(b))%27
+    # cipherText = cipherText + chr(v)
+    #print 'cipher='+ cipherText
+    return "".join(chr(ord(a)^ord(b)) for a,b in zip (key,message))
        # print v
        # print 'des message:'+chr((v ^ ord(b)))
     
@@ -42,9 +44,25 @@ def cipher(message):
 def main(argv):
     print 'Number of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
-    
-    padLength= argv[1]
-    numKey = argv[2]
+    try:
+        opts, args = getopt.getopt(argv,"he:k:",["encrypt=","genKey="])
+    except getopt.GetoptError:
+        print 'otp -e <encrypt> -k <genKey>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'otp -e <encrypt> -k <generatekey>'
+            sys.exit()
+        elif opt in ("-e", "--encrypt"):
+            print 'encrypt'
+            #           inputfile = arg
+        elif opt in ("-k", "--key"):
+            print 'generate key'
+      #          outputfile = arg
+   #print 'Input file is "', inputfile
+   #print 'Output file is "', outputfile
+    #padLength= argv[1]
+    #numKey = argv[2]
 #    genRandom(padLength,numKey)
-    cipher('hola')
-if __name__ == "__main__":main(sys.argv)# with if
+   # print cipher(cipher('hola'))
+if __name__ == "__main__":main(sys.argv[1:])# with if
