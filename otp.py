bdb7b7a5aff186ccd32e99585ee0c00b06e34070
saflores <saflores@idt-professionals.com>
2014-08-20 08:54:43 -0700

    added otp.py

---
+++ otp.py
@@ -0,0 +1,96 @@
+import binascii
+import sys
+import os
+import random
+import string
+import getopt
+
+def genRandom (padLength,numKey):
+    keyfile = open('keys','wb')
+    i = 0
+    print numKey
+    while i < int(numKey):
+        key=''
+        for j in range(0,int(padLength)):
+            key = key + random.choice(string.ascii)
+        keyfile.write(key+'\n')
+        print "ord"+ key
+        i+=1
+        print 'i:',i
+    keyfile.close()
+
+def xorC (m,k):
+    #we use the same method for encription and decription of the message
+    return "".join(chr(ord(a)^ord(b)) for a,b in zip (k,m))
+    
+def cipher(numMess):
+    keyfile = open ('keys','r')
+    messageFile = open('messages','wb')
+    j=0
+    while j < int(numMess):
+        message = raw_input('Enter message'+str(j)+':')
+        key = keyfile.readline()
+        
+        messageFile.write(xorC(message,key)+'\n')
+        j+=1
+        print message
+    messageFile.close()
+    keyfile.close()
+    
+def decipher(num):
+    keyfile = open('keys','r')
+    keyFileLines = keyfile.readlines()
+    keyfile.close()
+    
+    messageFile = open('messages','r')
+    messageFileLines = messageFile.readlines()
+    messageFile.close()
+
+    cMessage = messageFileLines[int(num)]
+    key = keyFileLines[int(num)]
+    message = xorC(key,cMessage)
+    
+    messageFile = open('messages','w')
+    keyfile = open('keys','w')
+    
+    j = 0
+    for line in messageFileLines:
+        if (j) != num:
+            messageFile.write(line)
+        j += 1
+    
+    j = 0
+    for line in keyFileLines:
+        if (j) != num:
+            keyfile.write(line)
+        j += 1
+
+    print message
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"he:k:d:",["encrypt=","genKey=","decrypt"])
+    except getopt.GetoptError:
+        print '\n-e <# of messages to encript> \n -k <# of keys to generate> <lenght of the keys> \n -d <message index>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print '-e <# of messages to encript> \n -k <pad length> <# of keys> \n -d <message index>'
+            sys.exit()
+        elif opt in ("-d","--decrypt"):
+            num =int(arg)-1
+            decipher(num)
+        elif opt in ("-e", "--encrypt"):
+            print 'encrypt'
+            numMess = arg 
+            cipher(numMess)
+    #       inputfile = arg
+            sys.exit()
+        elif opt in ("-k", "--genKey"):
+            padLength = arg
+            numKeys = argv[2]
+            print 'generate key'
+            print 'padlength='+padLength+'numkeys='+numKeys
+            genRandom(padLength,numKeys)
+            sys.exit()
+if __name__ == "__main__":main(sys.argv[1:])# with if
